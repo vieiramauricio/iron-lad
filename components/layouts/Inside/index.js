f6ac8465cba5a6204f0d011ddf45872f7ef13621
mauriciohike <mauriciorenanvs@gmail.com>
2020-08-21 16:39:27 -0300

    :lipstick: init layouts
    init layouts

---
+++ components/layouts/Inside/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Inside = () => {
+
+  return (
+    <Container>
+      <h1>Inside</h1>
+    </Container>
+  );
+};
+
+export default Inside;
---
+++ components/layouts/Outside/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Outside = () => {
+
+  return (
+    <Container>
+      <h1>Outside</h1>
+    </Container>
+  );
+};
+
+export default Outside;
