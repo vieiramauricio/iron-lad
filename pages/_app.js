3d26b0f7299ec7490d2c62b6c97d83c603bc48ac
mauriciohike <mauriciorenanvs@gmail.com>
2020-08-21 14:01:51 -0300

    :tada: init project
    init project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import GlobalStyle from '../styles/global';
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet"/>
+      </Head>
+    
+      <GlobalStyle/>
+      <Component {...pageProps} />  
+    </>
+  )
 }
 
 export default MyApp
